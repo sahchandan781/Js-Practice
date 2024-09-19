76758e5c9797d113a874979ccb1208355ec4f769
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2024-09-19 23:53:38 +0530

    first commit

---
+++ Objects/deStructuring.js
@@ -0,0 +1,10 @@
+const course = {
+    courseName : "js in english",
+    price : 999,
+    courseInstructure : "chandan"
+}
+
+const {courseInstructure : teacher} = course
+
+console.log(teacher);
+
